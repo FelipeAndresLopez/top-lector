14986a9a2db39f73856185e568e5fe5c8d63039a
Felipe Andres Lopez <felipe.lopezmoreno@baptisthealth.net>
2024-03-19 12:32:58 -0500

    fix some types

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,31 +3,19 @@ import { Container } from '../../components/Container/Container'
 
 import './styles.css'
 import { MENU_PATHS } from '../../const'
-import { useEffect, useState } from 'react'
+import { type FormEvent } from 'react'
 
 export const Login: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(false)
-
   const navigate = useNavigate()
 
-  // TODO: check if user is already logged in and redirect
-  useEffect(() => {
-    if (localStorage.getItem('user') !== null) {
-      navigate(MENU_PATHS.MY_PROFILE)
-      setIsLoading(false)
-    }
-  }, [isLoading])
-
-  const login = (event) => {
-    setIsLoading(true)
+  const handleLogin = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
-    const formData = new FormData(event.target)
+    const formData = new FormData(event.target as HTMLFormElement)
     const userCredentials = Object.fromEntries(formData)
     // TODO: call endpoint for login
     console.log(userCredentials)
     localStorage.setItem('user', JSON.stringify(userCredentials))
-
-    event.target.reset()
+    navigate(MENU_PATHS.MY_PROFILE)
   }
 
   return (
@@ -35,7 +23,7 @@ export const Login: React.FC = () => {
       <h1>Inicio de Sesión</h1>
 
       <div className='login'>
-        <form onSubmit={login}>
+        <form onSubmit={handleLogin}>
           <label>
             <p>Correo</p>
             <input type="email" required autoComplete='email' name="email" />
